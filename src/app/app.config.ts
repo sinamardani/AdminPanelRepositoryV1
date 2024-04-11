0b88fe97ed5fc2fd2c4aeb8fce72f7b3905bc71d
sinamardani <s.mardani2343@gmail.com>
2024-04-11 15:43:37 +0330

    Add nebular material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,47 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { NbMenuModule, NbThemeModule } from '@nebular/theme';
+import { NbRoleProvider, NbSecurityModule } from '@nebular/security';
+
+import { of as observableOf } from 'rxjs';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    importProvidersFrom(
+      NbThemeModule.forRoot(),
+      NbSecurityModule.forRoot({
+        accessControl: {
+          guest: {
+            view: '*',
+          },
+          user: {
+            parent: 'guest',
+            create: 'comments',
+            edit: 'comments',
+          },
+          admin: {
+            parent: 'user',
+            create: '*',
+            remove: '*',
+            edit: '*',
+          },
+        },
+      }),
+      NbMenuModule.forRoot()
+    ),
+    {
+      provide: NbRoleProvider,
+      useValue: {
+        getRole: () => {
+          // here we simply return a list of roles for current user
+          return observableOf('user');
+        },
+      },
+    },
+  ],
 };
