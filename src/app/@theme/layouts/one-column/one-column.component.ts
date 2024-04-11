d039367e60a1dddb78270fdc7eb58c9cd786a493
sinamardani <s.mardani2343@gmail.com>
2024-04-11 15:50:38 +0330

    Add base layout

---
+++ src/app/@theme/layouts/one-column/one-column.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import {
+  NbInputModule,
+  NbLayoutModule,
+  NbSidebarModule,
+  NbSidebarService,
+} from '@nebular/theme';
+
+@Component({
+  selector: 'app-one-column',
+  standalone: true,
+  providers: [NbSidebarService],
+  imports: [NbLayoutModule, NbSidebarModule, NbInputModule],
+  template: `
+    <nb-layout windowMode>
+      <nb-layout-header fixed>
+        <!-- <app-header></app-header> -->
+      </nb-layout-header>
+
+      <nb-sidebar right class="menu-sidebar" tag="menu-sidebar" responsive>
+        <ng-content select="nb-menu"></ng-content>
+      </nb-sidebar>
+
+      <nb-layout-column>
+        <ng-content select="router-outlet"></ng-content>
+      </nb-layout-column>
+
+      <nb-layout-footer fixed>
+        <!-- <ngx-footer></ngx-footer> -->
+      </nb-layout-footer>
+    </nb-layout>
+  `,
+  styleUrl: './one-column.component.scss',
+})
+export class OneColumnComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { OneColumnComponent } from './@theme/layouts/one-column/one-column.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'main',
+    pathMatch: 'full',
+  },
+  {
+    path: 'main',
+    component: OneColumnComponent,
+  },
+];
