7a4d3ef4d3fb99de2911c3a20242e486e39d212b
sina <s.mardani2343@gmail.com>
2024-04-14 18:44:03 +0330

    Add fontawsowm

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PagesComponent } from './pages.component';
-import { TestComponent } from './test/test.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: PagesComponent,
     children: [
-      {
-        path: 'dashboard',
-        component: TestComponent,
-      },
       {
         path: '',
         redirectTo: 'dashboard',
