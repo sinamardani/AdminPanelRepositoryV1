61742edf8612625d82bac47b9a2163ed7e1b3b38
sina <s.mardani2343@gmail.com>
2024-04-23 17:46:13 +0330

    S

--- src/app/pages/survey/viewsurvey/viewsurvey.component.ts
+++ src/app/pages/survey/viewsurvey/viewsurvey.component.ts
@@ -9,6 +9,7 @@ import {
   FormsModule,
   UntypedFormBuilder,
   UntypedFormGroup,
+  FormControl,
 } from '@angular/forms';
 import {
   NbActionsModule,
@@ -18,6 +19,29 @@ import {
   NbLayoutModule,
   NbStepperModule,
 } from '@nebular/theme';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatStepperModule } from '@angular/material/stepper';
 
 interface ControlModel {
   kind: 'input' | 'other';
@@ -71,11 +95,28 @@ enum ErrorMessage {
   imports: [
     ReactiveFormsModule,
     CommonModule,
-    NbStepperModule,
-    NbCardModule,
-    FormsModule,
-    NbButtonModule,
-    NbInputModule,
+    MatInputModule,
+    MatSelectModule,
+    MatAutocompleteModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    // MatBadgeModule,
+    // MatSidenavModule,
+    // MatListModule,
+    // MatCardModule,
+    // MatSliderModule,
+    // MatTableModule,
+    // MatPaginatorModule,
+    // MatSortModule,
+    // MatDatepickerModule,
+    // MatNativeDateModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    // MatDialogModule,
+    MatTooltipModule,
+    MatStepperModule,
   ],
   templateUrl: './viewsurvey.component.html',
   styleUrl: './viewsurvey.component.scss',
